36000cbf29b5fe00a72bfd048e599d110bbeaa68
nandanchakraborty <nandancsebubt@gmail.com>
2024-08-26 19:11:22 +0600

    merge 2 sorted array

---
+++ Merge2SortedArray.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+/*Problem statement: Given two sorted arrays arr1[] and
+arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order.
+Modify arr1 so that it contains the first N elements and modify arr2 so that it contains
+ the last M elements
+ */
+public class Merge2SortedArray {
+
+
+    public static void main(String[]args) {
+        
+
+    }
+}
