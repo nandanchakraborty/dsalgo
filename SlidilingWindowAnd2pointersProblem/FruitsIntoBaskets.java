9b51f16d6af9e2ac6c2caf9a074bd970f978b701
nandanchakraborty <nandancsebubt@gmail.com>
2025-07-28 09:29:45 +0600

    collect the maximum fruts from a row of tree

---
+++ SlidilingWindowAnd2pointersProblem/FruitsIntoBaskets.java
@@ -0,0 +1,47 @@
+package SlidilingWindowAnd2pointersProblem;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+
+public class FruitsIntoBaskets {
+
+    public static int fruitsOnbaskets(int[] ar, int k) {
+        int l = 0, r = 0, maxlen = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int len = ar.length;
+
+        while (r < len) {
+            mp.put(ar[r], mp.getOrDefault(ar[r], 0) + 1);
+
+            if (mp.size() > k) {
+                mp.put(ar[l], mp.get(ar[l]) - 1);
+                if (mp.get(ar[l]) == 0) {
+                    mp.remove(ar[l]);
+                }
+                l++;
+            }
+
+            if (mp.size() <= k) {
+                maxlen = max(maxlen, r - l + 1);
+            }
+
+            r++;
+        }
+
+        return maxlen;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int ans = fruitsOnbaskets(ar, k);
+        System.out.println("Highest number of fruits in the baskets is " + ans);
+    }
+}
