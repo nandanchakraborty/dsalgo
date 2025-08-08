946765019a911c0c0b5b449f21ba2ee009ef65bc
nandanchakraborty <nandancsebubt@gmail.com>
2025-08-08 12:03:21 +0600

    Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
    A subarray is a contiguous part of the array.

--- SlidingWindowAnd2pointersProblem/BinarySubArrayWithSumGoal.java
+++ SlidingWindowAnd2pointersProblem/BinarySubArrayWithSumGoal.java
@@ -1,4 +1,4 @@
-package SlidilingWindowAnd2pointersProblem;
+package SlidingWindowAnd2pointersProblem;
 
 import java.util.Scanner;
 
@@ -21,7 +21,7 @@ public class BinarySubArrayWithSumGoal {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int []ar = new int[n];
+        int []ar =  new int[n];
 
         for(int i=0;i<n;i++){
             ar[i] = sc.nextInt();
